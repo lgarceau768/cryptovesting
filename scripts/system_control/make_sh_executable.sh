8959c06a8b9a4d044a452b3d2d2bbd68a38bb7e9
Luke Garceau <lgarceau@uncc.edu>
2021-07-08 14:39:06 -0400

    added some scripts for monitoring the services

--- scripts/system_control/make_sh_executable.sh
+++ scripts/system_control/make_sh_executable.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-chmod u+x **/*.sh
+chmod 777 /home/fullsend/cryptovesting/app/front/start_server.sh
+chmod 777 /home/fullsend/cryptovesting/app/back/start_back.sh
+chmod 777 /home/fullsend/cryptovesting/scripts/rugmenot_contracts/service/start_tokens_listener.sh
