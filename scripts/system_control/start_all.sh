2eab06a9699ad38a03d21db1855af61e4a6dbd15
Luke Garceau <lgarceau@uncc.edu>
2021-07-08 14:19:21 -0400

    added some scripts for monitoring the services

---
+++ scripts/system_control/start_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo systemctl start manaul_entry.service
+sudo systemctl start manual_backend.service
+sudo systemctl start tokens_listener.service
---
+++ scripts/system_control/status_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo systemctl status manaul_entry.service
+sudo systemctl status manual_backend.service
+sudo systemctl status tokens_listener.service
