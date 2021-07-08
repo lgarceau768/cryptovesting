b3f64320fbe5cdaece691b0e0b9b397c4d9d3703
Luke Garceau <lgarceau@uncc.edu>
2021-07-08 14:18:06 -0400

    update to the scripts to run as fullsend

--- app/back/start_back.sh
+++ app/back/start_back.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/fullsend/cryptovesting/app/back
+su fullsend
 npm start
--- app/front/start_server.sh
+++ app/front/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/fullsend/cryptovesting/app/front
+su fullsend
 python3 -m http.server 5000
