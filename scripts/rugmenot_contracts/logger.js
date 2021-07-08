9671b528b4f799bc3814c5c33e2435369925065b
Luke Garceau <lgarceau@uncc.edu>
2021-07-08 14:40:33 -0400

    added some scripts for monitoring the services

--- scripts/rugmenot_contracts/logger.js
+++ scripts/rugmenot_contracts/logger.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const fs = require('fs')
 let pathUse = ""
 function init(path) {
@@ -16,23 +15,4 @@ _l = (data, level="LOG") => {
 
 module.exports = {
     init, _l
-=======
-const fs = require('fs')
-let pathUse = ""
-function init(path) {
-    pathUse = path
-    let isoString = new Date();
-    fs.writeFileSync(path, "[ START "+ isoString.toISOString() + " ] Starting app.js to listen to the sql and run contract checks\n")
-}
-
-_l = (data, level="LOG") => {
-    let isoString = new Date();
-    let line = "[ " + level + " " + isoString.toISOString() + " ] " + data + "\n";
-    fs.appendFileSync(pathUse, line)
-}
-
-
-module.exports = {
-    init, _l
->>>>>>> 1e3863f015956128c6e5f2af8081280410a04cc7
 }
