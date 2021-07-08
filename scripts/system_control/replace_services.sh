7dbcbaf3b80bac9babe0df047b41f312191fe6b7
Luke Garceau <lgarceau@uncc.edu>
2021-07-08 14:26:04 -0400

    added some scripts for monitoring the services

--- scripts/system_control/replace_services.sh
+++ scripts/system_control/replace_services.sh
@@ -2,3 +2,4 @@
 sudo cp /home/fullsend/cryptovesting/scripts/rugmenot_contracts/service/tokens_listener.service /lib/systemd/system/tokens_listener.service
 sudo cp /home/fullsend/cryptovesting/app/back/manual_backend.service /lib/systemd/system/manual_backend.service
 sudo cp /home/fullsend/cryptovesting/app/front/manaul.service /lib/systemd/system/manaul_entry.service
+sudo systemctl daemon-reload
