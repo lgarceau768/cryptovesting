d412d0761649907f664a59412a09ae52d90ecd51
Luke Garceau <lgarceau@uncc.edu>
2021-07-22 16:13:41 -0400

    working on the discord watcher bot to have constant updates also doing some reasearch on which addresses are used in the mainnet and how I can replicate a pancakepair token being minted

---
+++ app/discord_bot/app.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js')
+const http = require('http')
+const fetch = require('node-fetch')
+const { shared } = require('../worker_manager/workers/scripts/shared')
+const { init, _l } = require('../worker_manager/workers/scripts/logger')
+const path = require('path')
+const fs = require('fs')
+
+const client = new Discord.Client()
+const date = new Date()
+const logPath = path.join(__dirname, 'logs', 'discordBot_'+ date.getTime() + '.log')
+init(logPath, 'Cryptovesting Discord Bot')
+
+// INFO need to transfer env.json manually
+const IP = '192.168.1.224'//'25.89.250.119'
+const env = JSON.parse(fs.readFileSync(path.join(__dirname, 'env.json')))
+
+// INFO function to request events from backend
+const getEvents = async () => {
+    let data = {
+        host: 'http://'+IP+':4041',
+        path: '/pull_events',
+        method: 'GET'
+    }
+    const response = await fetch(data.host+data.path)
+    const json = await res.json()
+    const events = json['events']
+    console.log(events)
+}   
+
+// INFO setup function to loop for printing events to the channel
+setInterval(getEvents, 3000)
+
+client.on('ready', () => {
+    _l("Bot logged in as user "+client.user.tag, level="LOGIN")
+})
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong')
+    }
+})
+
+client.login(env.TOKEN)
+
