87dddbd9157fff4d980e2a9b3dcd136b825e0b7f
Luke Garceau <lgarceau@uncc.edu>
2021-07-23 11:21:13 -0400

    remote update

--- scripts/system_control/replace_services.sh
+++ scripts/system_control/replace_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo cp /home/fullsend/cryptovesting/app/back/manual_backend.service /lib/systemd/system/manual_backend.service
 sudo cp /home/fullsend/cryptovesting/app/front/manaul.service /lib/systemd/system/manaul_entry.service
-sudo cp /home/fullsend/cryptovesting/app/back/discord_bot.service /lib/systemd/system/discord_bot.service
+sudo cp /home/fullsend/cryptovesting/app/discord_bot/discord_bot.service /lib/systemd/system/discord_bot.service
 sudo cp /home/fullsend/cryptovesting/app/worker_manager/cryptovestingapp.service /lib/systemd/system/cryptovestingapp.service
 sudo systemctl daemon-reload
