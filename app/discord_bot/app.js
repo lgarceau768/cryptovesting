ac954eb34c739a39c7ce98a545296acdb370cebc
lgarceau <luke.garceau@ametek.com>
2021-09-22 23:20:04 -0400

    Fixing syntax errors

--- app/discord_bot/app.js
+++ app/discord_bot/app.js
@@ -400,6 +400,7 @@ async function requestThenSuccess(promiseFunction, functionality) {
     let startTime = Date.now()
     let returnVal = await promiseFunction();
     let endTime = Date.now()
+    _l('ReturnVal: '+returnVal)
     _l('Request finished: '+returnVal['success']+ ' functionality: '+functionality)
     if(returnVal['success']) {
         let messageRet = new Discord.MessageEmbed()
